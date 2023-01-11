4f89b486287a9c77aa24ce49c882911d77f62edd
PetruVamanu <petrisor_vmy@yahoo.com>
2023-01-12 01:35:27 +0200

    Fix array decl list + error on init with different type

--- utils.h
+++ utils.h
@@ -44,6 +44,7 @@ void insert_var(VariableList *varTable, Variable *newVar)
     
     strncpy(varTable->variables[varNum].name, newVar->name, MAX_VAR_LEN);
     strncpy(varTable->variables[varNum].scope, newVar->scope, MAX_SCOPE_LEN);
+    strncpy(varTable->variables[varNum].value, newVar->value, MAX_ARRAY_LEN);
     
     varTable->variables[varNum].line = newVar->line;
     varTable->variables[varNum].typeInfo.typeName = newVar->typeInfo.typeName;
